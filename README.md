e570471de9e394b9cd9a7a521763688c1f362710
lvl162 <loithui162@gmail.com>
2021-07-18 15:40:07 +0700

    add readme.md

---
+++ README.md
@@ -0,0 +1,12 @@
+# Introduction
+
+A reactjs landing page. 
+Package use: 
+    - styled-components 
+    - react-icons
+    - react-scroll
+
+The login func part is still being developed.
+
+# Demo
+You can check the [demo](https://duckduckgo.com).
--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages';
 import SignInPage from './pages/signin';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route path='/' component={Home} exact />
         <Route path='/signin' component={SignInPage} exact />
